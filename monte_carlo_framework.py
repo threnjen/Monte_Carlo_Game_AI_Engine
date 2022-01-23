76e75b914ad2f72aa44f9c6657b735f019a66e48
threnjen <61851526+threnjen@users.noreply.github.com>
2022-01-23 14:51:28 -0800

    prepare to integrate new updates

--- monte_carlo_framework.py
+++ monte_carlo_framework.py
@@ -313,5 +313,5 @@ class Player():
 
 players = 2
 game = GameEngine(players)
-#game.play_game_byturns(simulations = 100)
-game.play_entire_game(simulations = 1000, game_label='array_1000')
+game.play_game_byturns(simulations = 10000)
+#game.play_entire_game(simulations = 1000, game_label='array_1000')
