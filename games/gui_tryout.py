b446fe775266af1cd2386146ec33b6ba42dd9beb
Andrew Herrmann <aherrmann85@gmail.com>
2023-09-16 17:15:46 -0700

    Added tile container as pydantic class

--- games/gui_tryout.py
+++ games/gui_tryout.py
@@ -3,26 +3,6 @@ import PySimpleGUI as sg
 # First the window layout in 2 columns
 
 
-# file_list_column = [
-#     [
-#         sg.Text("Image Folder"),
-#         sg.In(size=(1, 1), enable_events=True, key="-FOLDER-"),
-#         sg.FolderBrowse(),
-#     ],
-#     [
-#         sg.Listbox(
-#             values=[], enable_events=True, size=(40, 20), key="-FILE LIST-"
-#         )
-#     ],
-# ]
-
-# # For now will only show the name of the file that was chosen
-# image_viewer_column = [
-#     [sg.Text("Choose an image from list on left:")],
-#     [sg.Text(size=(40, 1), key="-TOUT-")],
-#     [sg.Image(key="-IMAGE-")],
-# ]
-
 def display_factory(factory_dict: dict):
     factory_prefix = "fact_"
     sg_col_array = []
