58744058e20056a3f5a7e4a9e5ac19eb82bbfe1e
Jenny Wadkins <threnjen@gmail.com>
2023-11-05 20:41:28 -0800

    define selfs in init

--- games/dungeon_crawler/enemy.py
+++ games/dungeon_crawler/enemy.py
@@ -1,11 +1,12 @@
 class Enemy:
-    def __init_subclass__(cls) -> None:
-        pass
+    def __init__(self) -> None:
+        self.enemy_deck = self.create_enemy_deck()
+        print(self.enemy_deck)
 
     def create_enemy_deck(self):
-        self.enemy_deck = ["S1", "S2", "S3", "Move", "Attack"]
-        self.enemy_deck *= 3
-        print(self.enemy_deck)
+        enemy_deck = ["S1", "S2", "S3", "Move", "Attack"]
+        enemy_deck *= 3
+        return enemy_deck
 
 
 if __name__ == "__main__":
--- games/dungeon_crawler/player.py
+++ games/dungeon_crawler/player.py
@@ -1,9 +1,13 @@
 class Player:
-    def __init_subclass__(cls) -> None:
-        pass
+    def __init__(self):
+        self.player_deck = self.create_deck()
 
     def create_deck(self):
-        self.player_deck = ["Attack", "Attack", "Attack", "Move", "Move"]
+        player_deck = ["Attack", "Attack", "Attack", "Move", "Move"]
         max_deck_size = 12
-        while len(self.player_deck) < max_deck_size:
+        while len(player_deck) < max_deck_size:
             pass
+        return player_deck
+
+    def avail_start_cards(self):
+        avail_start_cards = ["Attack", "Move", "Recover"]
