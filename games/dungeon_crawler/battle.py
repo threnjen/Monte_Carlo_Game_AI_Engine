09065efdb2d88efc4f87273e60728bd1cf137fbc
Andrew Herrmann <aherrmann85@gmail.com>
2023-11-06 20:20:15 -0800

    Added skeleton of update game

--- games/dungeon_crawler/battle.py
+++ games/dungeon_crawler/battle.py
@@ -13,7 +13,6 @@ class Battle(BaseGameObject):
     battle_grid: BattleGrid = Field(default_factory=BattleGrid, validate_default=True)
     round_actions: list[DungeonCrawlerAction] = None
 
-
     @field_validator("battle_grid")
     def create_battle_grid(self):
         if self.battle_grid is None:
@@ -27,7 +26,7 @@ class Battle(BaseGameObject):
         self.actors = dict(sorted(self.actors.items(), key=lambda x: x[1].initiative))
 
     @property
-    def actions_this_round(self) -> int:
+    def actions_required_this_round(self) -> int:
         """The game needs to know the number of actions each actor will be able to program.
         Right now, this is dictated by the undead actors, and is the max among those undead
         actors."""
@@ -39,32 +38,43 @@ class Battle(BaseGameObject):
         we can't just play out the whole round without getting more input from the player."""
         pass
 
-
     @property
     def current_player(self):
         return self.players[self.current_player_num]
 
-    def get_available_actions(self, special_policy: bool = False) -> list:
-        pass
+    @property
+    def all_round_actions_collected(self) -> bool:
+        """A boolean to determine if all players have programmed their actions for the round."""
+        return len(self.round_actions) == self.actions_required_this_round * len(self.human_player_count)
 
-    # def get_adjacent_targets(self, actor_num: int) -> list:
-    #     adjacent_squares = self.battle_grid.get_adjacent_squares(actor_num)
-    #     return [self.battle_grid.get_actor(square) for square in adjacent_squares]
+    def get_available_actions(self, special_policy: bool = False) -> list[DungeonCrawlerAction]:
+        """This is the function that the game will call to get the available actions for the current player.
+        There are two possibilities:
+            - The player is programming their actions for the round.  In this case, the player will be able to
+            choose from a list of card permutations.
+            - The player is choosing a direction to enact their chosen action.  They need the battle grid to
+            determine adjacent empty and occupied squares."""
+        if not self.all_round_actions_collected:
+            return self.current_player.get_available_actions(self.actions_required_this_round)
+        else:
+            return self.current_player.get_available_directions(self.battle_grid)
 
-    def get_distance(self, position1, position2) -> int:
-        indices_element1 = np.argwhere(my_2d_array == position1)  # Get the indices of element1
-        indices_element2 = np.argwhere(my_2d_array == element2)  # Get the indices of element2
-
-        if indices_element1.size > 0 and indices_element2.size > 0:
-            position1 = indices_element1[0]
-            position2 = indices_element2[0]
-            positional_difference = np.abs(position2 - position1)
-
-    def update_game_with_action(self, action) -> None:
-        if len(self.round_actions) < self.actions_this_round * len(self.human_player_count):
+    def update_game_with_action(self, action: DungeonCrawlerAction) -> None:
+        if not self.all_round_actions_collected:
             self.round_actions.append(action)
             return
-        self.current_player.
+        # Something about this feels wrong.
+        # I think the right move is to pass the action to the player, and let the player
+        # decide what to do with it.
+        if action.type == "move":
+            self.current_player.move(action)
+        elif action.type == "attack":
+            self.current_player.attack(action, self.actors)
+        elif action.type =="defend":
+            self.current_player.defend(action)
+        elif action.type == "recover":
+            self.current_player.recover(action)
+
 
     def is_game_over(self) -> bool:
         pass
